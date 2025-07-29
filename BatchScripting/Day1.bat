173340fe31da70981db288483e58d3975ba9c3d4
Arjun3000 <arjunsharma.cse2023.gna@gmail.com>
2025-07-29 17:16:02 +0530

    adding day 2

--- BatchScripting/Day1.bat
+++ BatchScripting/Day1.bat
@@ -1,3 +1,6 @@
 @echo off
 echo hello, this is first day
 pause
+
+@echo off
+echo hello world
---
+++ BatchScripting/Day2.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set /p name=enter your name : 
+
+rem now use echo.
+
+echo.
+echo hello, %name% !, this your pc
+echo.
+pause
