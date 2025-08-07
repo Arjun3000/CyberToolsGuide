46da715d2a2a14694b0d113df0c157d233db9b14
Arjun3000 <arjunsharma.cse2023.gna@gmail.com>
2025-08-07 18:17:53 +0530

    adding the files

---
+++ BatchScripting/Day11.bat
@@ -0,0 +1,13 @@
+@echo off
+echo %USERNAME%
+echo %USERPROFILE%
+echo %APPDATA%
+rem echo. > %TEMP%/ech.txt
+
+::echo %PATH%
+pause
+echo %COMPUTERNAME%
+echo %LOCALAPPDATA%
+echo %SYSTEMROOT%
+echo %PUBLIC%
+echo %PROGRAMFILES%
